2fb63151ab93493be5d4dfd8689208c272075a19
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-11-05 16:25:41 +0100

    feat: created initial backend files and endpoints
    created initial files for backend and added endpoint sockets

---
+++ images/backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server, Socket } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const Port = process.env.PORT || 3000;
+const io = new Server(server)
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('webrtc backend is running')
+});
+
+server.listen(Port, () => {
+    console.log(`server is running on  http://localhost:${Port}`)
+});
+
+io.on('connection', (socket: Socket) => {
+
+    socket.emit('me', socket.id)
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('callended')
+    })
+
+    socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit('calluser', { signal: signalData, from, name })
+    })
+
+    socket.on('answercall', (data) => {
+        io.to(data.to).emit('callaccepted', data.signal)
+    })
+})
