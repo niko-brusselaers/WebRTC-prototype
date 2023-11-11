d854cccf19bb7dd3aa2e5aa609059796737ffc20
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-11-11 16:07:34 +0100

    feat: added user calling functionality
    added functionality to call other user by username

--- images/frontend/webrtc/src/core/pages/meeting/Meeting.tsx
+++ images/frontend/webrtc/src/core/pages/meeting/Meeting.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useEffect, useRef, useState } from "react";
 import { io,Socket } from "socket.io-client";
+import Peer from "simple-peer";
 import ILoginResponse from "../../shared/interfaces/IResponses";
 
 const socket: Socket = io("localhost:4000");
@@ -16,6 +17,16 @@ function Meeting() {
     const [stream, setStream] = useState<MediaStream | undefined>(undefined);
     const myVideo = useRef<HTMLVideoElement | null>(null)
 
+    //other user ref
+    const userVideo = useRef<HTMLVideoElement | null>(null)
+
+    //call usestate
+    const [notification, setNotification] = useState<string | undefined>(undefined);
+    const [usernameToCall, setUsernameToCall] = useState<string | undefined>("");
+    const [call, setCall] = useState<{ from: string, name: string, signal: any } | undefined>(undefined);
+    const connectionRef = useRef<Peer.Instance | null>(null);
+
+
 
     //select username and emit to server
     const selectUsername = (event: FormEvent) => {
@@ -23,6 +34,59 @@ function Meeting() {
         socket.emit("login", {newUser: usernameInput});
     }
 
+    const callUser = (event: FormEvent) => {
+        event.preventDefault();
+        if (usernameToCall) {
+            console.log("make call");
+        
+            const peer = new Peer({ initiator: true, trickle: false, stream });
+
+            peer.on('signal', (data) => {
+                socket.emit('callUser', { userToCall: usernameToCall, signalData: data, from: username });
+                setCall({ from: username!, name: usernameToCall, signal: data });
+            });
+
+            peer.on('stream', (currentStream) => {
+                console.log("stream");
+                if (userVideo.current) {
+                    userVideo.current.srcObject = currentStream;
+                }
+            });
+
+            socket.on('callAccepted', (signal) => {
+                // Move this line after setting up the 'signal' event listener
+                peer.signal(signal);
+            });
+
+            connectionRef.current = peer;
+        }
+    };
+
+    const acceptCall = () => {
+        // Create a new SimplePeer instance for the accepted call
+        const peer = new Peer({ initiator: false, trickle: false, stream });
+
+        peer.on('signal', (data) => {
+            socket.emit('answerCall', { signal: data, to: call!.from });
+        });
+
+        peer.on('stream', (currentStream) => {
+            if (userVideo.current) {
+                userVideo.current.srcObject = currentStream;
+            }
+        });
+
+        console.log(call);
+
+        // Check the signalingState before calling signal
+            if (call) {
+                peer.signal(call.signal);
+                connectionRef.current = peer;
+            }
+
+    };
+
+    //get user media and set stream to video element
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then((currentStream) => {
@@ -38,6 +102,7 @@ function Meeting() {
 
     },[]);
 
+    //listen for any incoming socket events
     useEffect(() => {
 
         //listen for login event
@@ -53,6 +118,32 @@ function Meeting() {
             }
         });
 
+        socket.on('callUser', (data:any) => {
+            // set the call data to state
+            setCall({ from: data.from, name: username!, signal: data.signal })
+            // set the notification data to state
+            setNotification(`You have a call from ${data.from}`);
+            
+        });
+
+        socket.on('callAccepted', (signal) => {
+            // Create a new SimplePeer instance for the accepted call
+            const peer = new Peer({ initiator: true, trickle: false, stream });
+
+            // Handle the signal from the accepted call
+            peer.signal(signal);
+
+            // Set up event listeners for the peer connection
+            peer.on('stream', (currentStream) => {
+                if (userVideo.current) {
+                    userVideo.current.srcObject = currentStream;
+                }
+            });
+
+            // Save the peer connection in the useRef for later reference
+            connectionRef.current = peer;
+        });
+
     },[]);
 
     
@@ -64,16 +155,37 @@ function Meeting() {
             }
 
             {username ? 
-            
+                <>
                 <h2>Welcome {username}</h2> 
+            
+                <form onSubmit={callUser}>
+                    <input type="text" placeholder="Enter username" onChange={(e) => setUsernameToCall(e.target.value)}/>
+                    <button type="submit"  >Call</button>
+                </form>
+                </>
                 : 
                 <form onSubmit={selectUsername}>
                         <input type="text" placeholder="Enter your name" onChange={(e) => setUsernameInput(e.target.value)} />
                         <button type="submit">Submit</button>
                 </form>
             }
+
+            {notification ? 
+            <>
+                <p>{notification}</p> 
+                <div>
+                    <button onClick={acceptCall}>accept</button>
+                </div>
+            </>
+
+            : 
+            
+            null
+            
+            }
             
             <video ref={myVideo} muted autoPlay style={{ width: "300px" }}></video>
+            <video ref={userVideo} muted autoPlay style={{ width: "300px" }}></video>
 
         </div>
      );
