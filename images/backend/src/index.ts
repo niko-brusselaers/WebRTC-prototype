af08fa5fcebf5db9c6c40e63378d90c5a7e08e2d
niko.brusselaers <niko.brusselaers@student.ehb.be>
2023-11-11 17:11:30 +0100

    feat: added login socket endpoint
    added login socket endpoint that checks if the username has been taken, if not it will store the username and its corresponding socketID and send a response back

--- images/backend/src/index.ts
+++ images/backend/src/index.ts
@@ -2,15 +2,24 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import { Server, Socket } from 'socket.io';
+import { IUser } from './shared/interfaces/IUser';
 
 const app = express();
 const server = http.createServer(app);
-const Port = process.env.PORT || 3000;
-const io = new Server(server)
+const Port = process.env.PORT || 4000;
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+// variable to store users
+const users:IUser[] = [];
 
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (req:any, res:any) => {
     res.send('webrtc backend is running')
 });
 
@@ -19,14 +28,49 @@ server.listen(Port, () => {
 });
 
 io.on('connection', (socket: Socket) => {
+    console.log("connection", socket.id);
+    
 
-    socket.emit('me', socket.id)
+    socket.on('login', (data) => {
+        // check if user already exists
+        if (users.find(user => user.username === data.newUser) === undefined 
+            && users.find(user => user.id === socket.id) === undefined) 
+        {
+            // add new user to users array
+            users.push(
+                {
+                    id: socket.id,
+                    username: data.newUser
+                }
+            );
+            let response = {
+                data: "new user added",
+                username: data.newUser,
+                statuscode: 200,
+            }
+            // send response to client
+            socket.emit('login', response);
+
+        } else {
+            let response = {
+                data: "this username has been taken",
+                statuscode: 401
+            }
+            // send error response to client
+            socket.emit('login', response);
+
+
+        }        
+
+    })
 
     socket.on('disconnect', () => {
         socket.broadcast.emit('callended')
     })
 
     socket.on('calluser', ({ userToCall, signalData, from, name }) => {
+        console.log('calluser', userToCall, signalData, from, name);
+        
         io.to(userToCall).emit('calluser', { signal: signalData, from, name })
     })
 
